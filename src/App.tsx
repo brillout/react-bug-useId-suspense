8a97a06d79ad04720119150112b92738f229a006
Romuald Brillout <git@brillout.com>
2022-06-03 12:12:38 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useId } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <React.Suspense fallback={<p>I'm lazy loaded...</p>}>
+      <LazyComponent/>
+    </React.Suspense>
   )
 }
 
-export default App
+let cache: any = {}
+function LazyComponent() {
+  console.log('<LazyComponent/>')
+  const id = useId();
+  console.log('id: ' + id)
+  let entry = cache[id]
+  console.log('entry: ' + entry)
+
+  if( entry?.promise ) throw entry.promise;
+
+  if( !entry ) {
+    entry = cache[id] ??= {}
+    let resolve: Function;
+    const promise = new Promise(r => resolve = r)
+    setTimeout(() => {
+      delete cache[id].promise;
+      cache[id].done = true
+      resolve()
+    }, 2000)
+    cache[id].promise = promise
+    throw promise
+  }
+
+  return <>hello</>
+}
