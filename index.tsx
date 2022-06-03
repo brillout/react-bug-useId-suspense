4fb4913021e9b1ed792a5fae5880791454ba224b
Romuald Brillout <git@brillout.com>
2022-06-03 12:56:51 +0200

    showcase `useId()` working with `useState()`

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from 'react'
+import React, { useId, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -8,40 +8,16 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 )
 
 function App() {
-  return (
-    <React.Suspense fallback={<p>I'm lazy loaded...</p>}>
-      <LazyComponent />
-    </React.Suspense>
-  )
+  return <Counter />
 }
 
-let cache: { promise?: Promise<void>; done?: true } = {}
-function LazyComponent() {
-  console.log('<LazyComponent/>')
-
-  // Doesn't work:
+function Counter() {
+  const [count, setCount] = useState(0)
   const id = useId()
-  // Works:
-  //const id = 'some-static-id'
-
-  let entry = (cache[id] ??= {})
-  console.log('id: ', id)
-  console.log('entry: ', entry)
-
-  if (entry.promise) {
-    throw entry.promise
-  } else if (!entry.done) {
-    let resolve: Function
-    const promise = new Promise((r) => (resolve = r))
-    entry.promise = promise
-    setTimeout(() => {
-      delete cache[id].promise
-      cache[id].done = true
-      resolve()
-    }, 2000)
-    console.log('promise: ', promise)
-    throw promise
-  }
-
-  return <p>Hello</p>
+  return (
+    <>
+      <button onClick={() => setCount(count + 1)}>{count}</button>
+      <div>id: {id}</div>
+    </>
+  )
 }
