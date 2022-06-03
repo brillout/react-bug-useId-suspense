5649b6854fff90ddb9d14c53fb947832d95c9b3c
Romuald Brillout <git@brillout.com>
2022-06-03 12:16:35 +0200

    wip

--- index.tsx
+++ index.tsx
@@ -1,9 +1,16 @@
-import React, { useId } from "react";
+import React, { useId } from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
 
 function App() {
   return (
     <React.Suspense fallback={<p>I'm lazy loaded...</p>}>
-      <LazyComponent/>
+      <LazyComponent />
     </React.Suspense>
   )
 }
@@ -11,19 +18,19 @@ function App() {
 let cache: any = {}
 function LazyComponent() {
   console.log('<LazyComponent/>')
-  const id = useId();
+  const id = useId()
   console.log('id: ' + id)
   let entry = cache[id]
   console.log('entry: ' + entry)
 
-  if( entry?.promise ) throw entry.promise;
+  if (entry?.promise) throw entry.promise
 
-  if( !entry ) {
+  if (!entry) {
     entry = cache[id] ??= {}
-    let resolve: Function;
-    const promise = new Promise(r => resolve = r)
+    let resolve: Function
+    const promise = new Promise((r) => (resolve = r))
     setTimeout(() => {
-      delete cache[id].promise;
+      delete cache[id].promise
       cache[id].done = true
       resolve()
     }, 2000)
